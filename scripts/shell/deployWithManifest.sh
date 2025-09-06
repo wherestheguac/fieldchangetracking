b0bad48bccd8e6474e208a356d80c25ea5efcab8
wherestheguac <seriff@protonmail.com>
2025-09-06 17:02:46 -0400

    Update deployWithManifest.sh

--- scripts/shell/deployWithManifest.sh
+++ scripts/shell/deployWithManifest.sh
@@ -4,9 +4,6 @@ sf package install --wait 20 --security-type AdminsOnly --package 04tKe0000011N4
 # ****Assign Nebula Logger Admin Permission Set to Current User****
 sf org assign permset --name "LoggerAdmin"
 
-# ****Set Remove Global Value Sets From Picklist Fields Flag****
-export REMOVE_GLOBAL_VALUE_SETS=false
-
 # ****Deploy Source to Org Using Manifest****
 sf project deploy start --manifest manifest/package.xml --ignore-conflicts
 
